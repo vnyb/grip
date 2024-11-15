840d15d2dc10fd7039e1242a28a0b967a922b2eb
Vianney Bajart <vianney.bajart@protonmail.com>
2024-11-15 11:09:35 +0100

    date helpers

--- grip/__init__.py
+++ grip/__init__.py
@@ -1,4 +1,5 @@
 import datetime
+import dateutil.parser
 import json
 import logging
 import os
@@ -70,5 +71,15 @@ yaml.add_representer(str, yaml_str_representer)
 def now() -> datetime.datetime:
     return datetime.datetime.now().astimezone()
 
-def date_to_str(dt: datetime.datetime) -> str:
-    return dt.strftime("%Y-%m-%d")
+def date_to_str(date: datetime.date | datetime.datetime) -> str:
+    return date.strftime("%Y-%m-%d")
+
+def parse_iso_date(string: str) -> datetime.date:
+    if len(string) == 7:
+        string = string + "-01"
+    return datetime.datetime.strptime(string, "%Y-%m-%d").date()
+
+def parse_iso_datetime(string: str) -> datetime.datetime:
+    if len(string) == 7:
+        string = string + "-01"
+    return dateutil.parser.isoparse(string).astimezone()
