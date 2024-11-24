4fff995d3bad8f3f0ffdbab280f2a17a7d769f1f
Vianney Bajart <vianney.bajart@protonmail.com>
2024-11-24 11:22:55 +0100

    toml helper

--- grip/__init__.py
+++ grip/__init__.py
@@ -5,6 +5,7 @@ import json
 import logging
 import os
 import sys
+import tomllib
 import yaml
 
 from typing import Callable, NoReturn, Tuple, TypeVar
@@ -51,18 +52,22 @@ def get_file_age(path: str) -> datetime.timedelta:
     creation_date = datetime.datetime.fromtimestamp(timestamp)
     return datetime.datetime.now() - creation_date
 
+
 def get_file_staleness(path: str) -> datetime.timedelta:
     timestamp = os.path.getmtime(path)
     last_modified_date = datetime.datetime.fromtimestamp(timestamp)
     return datetime.datetime.now() - last_modified_date
 
-def read_file(path: str) -> str:
-    with open(path, 'r') as file:
+
+def read_file(path: str, mode="r") -> str:
+    with open(path, mode) as file:
         return file.read()
 
+
 def write_file(data, path: str):
-    with open(path, 'w') as file:
+    with open(path, "w") as file:
         return file.write(data)
+
 
 def read_last_line(fp: io.BufferedReader, ignore_empty_lines=False) -> str | None:
     fp.seek(0, 2)  # Move to the end of the file
@@ -78,7 +83,7 @@ def read_last_line(fp: io.BufferedReader, ignore_empty_lines=False) -> str | Non
     while pos > 0:
         char = read()
 
-        if char == b'\n':
+        if char == b"\n":
             if end - start == 0 and ignore_empty_lines:
                 end = start = pos
                 continue
@@ -93,45 +98,60 @@ def read_last_line(fp: io.BufferedReader, ignore_empty_lines=False) -> str | Non
     fp.seek(start)
     return fp.read(n).decode()
 
+
 def read_json(path: str):
-    with open(path, 'r') as file:
+    with open(path, "r") as file:
         return json.load(file)
 
+
 def write_json(data, path: str, indent=True):
-    with open(path, 'w') as file:
+    with open(path, "w") as file:
         return json.dump(data, file, indent=indent)
 
-        
+
 def read_yaml(path: str):
     with open(path, "r", encoding="utf-8") as file:
         return yaml.safe_load(file)
 
+
 def save_yaml(data, path: str):
-    with open(path, "w", encoding='utf-8') as file:
+    with open(path, "w", encoding="utf-8") as file:
         yaml.dump(data, file, allow_unicode=True, sort_keys=False)
 
+
 def yaml_str_representer(dumper, data):
-    if '\n' in data:  # Si la chaîne contient plusieurs lignes
-        return dumper.represent_scalar('tag:yaml.org,2002:str', data, style='|')
-    return dumper.represent_scalar('tag:yaml.org,2002:str', data)
+    if "\n" in data:  # Si la chaîne contient plusieurs lignes
+        return dumper.represent_scalar("tag:yaml.org,2002:str", data, style="|")
+    return dumper.represent_scalar("tag:yaml.org,2002:str", data)
+
 
 yaml.add_representer(str, yaml_str_representer)
+
+
+def read_toml(path: str) -> dict:
+    with open(path, "rb") as file:
+        return tomllib.load(file)
+
 
 def now() -> datetime.datetime:
     return datetime.datetime.now().astimezone()
 
+
 def date_to_str(date: datetime.date | datetime.datetime) -> str:
     return date.strftime("%Y-%m-%d")
+
 
 def parse_iso_date(string: str) -> datetime.date:
     if len(string) == 7:
         string = string + "-01"
     return datetime.datetime.strptime(string, "%Y-%m-%d").date()
 
+
 def parse_iso_datetime(string: str) -> datetime.datetime:
     if len(string) == 7:
         string = string + "-01"
     return dateutil.parser.isoparse(string).astimezone()
+
 
 def deep_dict_equal(a: dict, b: dict) -> bool:
     for key in a.keys():
