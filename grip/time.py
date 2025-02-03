d65c27472f2cd20acd32490922aa74e7a55c2ac3
Vianney Bajart <vianney.bajart@protonmail.com>
2025-02-03 19:32:27 +0100

    time: duration validator

--- grip/time.py
+++ grip/time.py
@@ -1,5 +1,7 @@
 import datetime
 import dateutil.parser
+import pytimeparse.timeparse
+from pydantic_core import core_schema
 
 
 def now_tz() -> datetime.datetime:
@@ -20,3 +22,28 @@ def parse_iso_datetime(string: str) -> datetime.datetime:
     if len(string) == 7:
         string = string + "-01"
     return dateutil.parser.isoparse(string).astimezone()
+
+
+class TimeDelta(datetime.timedelta):
+    @classmethod
+    def validate(cls, value) -> datetime.timedelta:
+        try:
+            value = float(value)
+        except ValueError:
+            pass
+
+        if isinstance(value, int) or isinstance(value, float):
+            seconds = value
+        else:
+            seconds = pytimeparse.timeparse.timeparse(value)
+            if seconds is None:
+                raise ValueError("invalid time delta")
+
+        return datetime.timedelta(seconds=seconds)
+
+    @classmethod
+    def __get_pydantic_core_schema__(cls, source, handler):
+        """
+        Define the Pydantic core schema for validation.
+        """
+        return core_schema.no_info_plain_validator_function(cls.validate)
